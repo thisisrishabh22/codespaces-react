97cf94a644490a4549abda2c7de19eabce757b63
Rishabh Singh <56874272+thisisrishabh22@users.noreply.github.com>
2023-04-19 16:58:49 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -12,14 +12,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Hello
         </p>
       </header>
     </div>
